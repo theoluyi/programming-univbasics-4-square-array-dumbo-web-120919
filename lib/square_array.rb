de656b4df3f8d408395666ecc91cd9d617043ec7
Theo Carney <theoluyi@gmail.com>
2019-11-29 02:13:10 +0000

    Automatically backed up by Learn

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -3,7 +3,7 @@ def square_array(array)
   counter = 0
   new_array = []
   while array[counter] do
-    new_array.push(array[counter]**2)
+    new_array.push((array[counter])**2)
     counter +=1
   end 
 end
